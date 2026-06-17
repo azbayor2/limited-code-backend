4cad8ad31c5f01ecaa4ac85b00bb87de11c53903
azbayor2 <hanharry2@gmail.com>
2026-06-17 14:49:59 +0900

    feat: reponse entity에 반환값 추가

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,31 @@
-import { Controller, Query, Get, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Query,
+  Get,
+  Post,
+  Body,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
 // import { User } from './entity/User.entity';
 // import { EmailVerification } from 'src/auth/entity/EmailVerification.entity';
 import { UserService } from './user.service';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
-import { CheckUserExists, FindUserArgs, RegisterUserDto } from './user.dto';
+import {
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
+import {
+  CheckUserExists,
+  CheckUserExistsReponseDto,
+  FindUserArgs,
+  FindUserResponseDto,
+  RegisterUserDto,
+  RegisterUserResponseDTO,
+  UserResponseInfo,
+} from './user.dto';
+import { User } from './entity/User.entity';
 
 @ApiTags('users')
 @Controller('/user')
@@ -17,26 +39,56 @@ export class UserController {
   @ApiOperation({
     description: '사용자가 존재하는지 확인합니다',
   })
+  @ApiOkResponse({
+    description: '성공했을 때 응답입니다.',
+    type: CheckUserExistsReponseDto,
+  })
+  @HttpCode(HttpStatus.OK)
   @Get('/exists')
-  async exists(@Query() CheckUserExists: CheckUserExists): Promise<object> {
+  async exists(
+    @Query() CheckUserExists: CheckUserExists,
+  ): Promise<CheckUserExistsReponseDto> {
     return {
-      result: await this.userService.check(CheckUserExists),
+      success: await this.userService.check(CheckUserExists),
     };
   }
 
   @ApiOperation({
     description: '사용자를 id, 이메일, username으로 조회합니다',
   })
+  @ApiOkResponse({
+    description: '성공 시 반환입니다',
+    type: FindUserResponseDto,
+  })
   @Get('/')
-  async get(@Query() findUserArgs: FindUserArgs) {
-    return this.userService.find(findUserArgs);
+  async get(@Query() findUserArgs: FindUserArgs): Promise<FindUserResponseDto> {
+    const user: User | null = await this.userService.find(findUserArgs);
+    const userInfo: UserResponseInfo | undefined = user
+      ? {
+          id: user.id,
+          email: user.email,
+          username: user.username,
+        }
+      : undefined;
+
+    return {
+      success: user ? true : false,
+      userInfo: userInfo,
+    };
   }
 
   @ApiOperation({
     description: '회원가입을 진행합니다',
   })
+  @ApiCreatedResponse({
+    description: '성공 응답을 반환합니다',
+    type: RegisterUserResponseDTO,
+  })
+  @HttpCode(HttpStatus.CREATED)
   @Post('/register')
-  async register(@Body() registerUserDto: RegisterUserDto) {
-    return await this.userService.register(registerUserDto);
+  async register(
+    @Body() registerUserDto: RegisterUserDto,
+  ): Promise<RegisterUserResponseDTO> {
+    return { success: await this.userService.register(registerUserDto) };
   }
 }
--- src/user/user.dto.ts
+++ src/user/user.dto.ts
@@ -2,6 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsNumber, IsOptional, IsString } from 'class-validator';
 
+/** 사용자 정보 검색 api */
 export class FindUserArgs {
   @ApiProperty({
     example: 1,
@@ -32,6 +33,35 @@ export class FindUserArgs {
   username?: string | undefined;
 }
 
+export class UserResponseInfo {
+  @ApiProperty({
+    description: 'pk를 반환합니다.',
+  })
+  id: number;
+  @ApiProperty({
+    description: '닉네임을 반환합니다',
+  })
+  username: string;
+  @ApiProperty({ description: ' 이메일을 반환합니다.' })
+  email: string;
+}
+
+export class FindUserResponseDto {
+  @ApiProperty({
+    example: true,
+    description: '조회 성공 여부를 반환합니다',
+    required: true,
+  })
+  success: boolean;
+  @ApiProperty({
+    description: 'success가 true이면 정보를 반환합니다',
+    required: false,
+    type: UserResponseInfo,
+  })
+  userInfo: UserResponseInfo | undefined;
+}
+
+/** 사용자 존재 유무 검색 api */
 export class CheckUserExists {
   @ApiProperty({
     example: 'example@gmail.com',
@@ -52,6 +82,16 @@ export class CheckUserExists {
   username?: string;
 }
 
+export class CheckUserExistsReponseDto {
+  @ApiProperty({
+    example: true,
+    description: '성공 여부를 반환합니다',
+    required: true,
+  })
+  success: boolean;
+}
+
+/** 사용자 회원가입 api */
 export class RegisterUserDto {
   @ApiProperty({
     example: 'example@gmail.com',
@@ -85,4 +125,13 @@ export class RegisterUserDto {
   })
   @IsString()
   username: string;
+}
+
+export class RegisterUserResponseDTO {
+  @ApiProperty({
+    example: true,
+    description: '성공 여부를 반환합니다',
+    required: true,
+  })
+  success: boolean;
 }
